bafaca16af3f281c6d91b7a19fe239f79ae2056c
krashLanded <ericquesada02@icloud.com>
2021-12-06 17:31:32 -0800

    input functiont test

---
+++ library.js
@@ -0,0 +1,12 @@
+
+    let title = '';
+
+    function named() {
+        alert(userInput.value);
+    }
+    function getTitle() {
+        title = document.getElementById('title').value;
+        alert(title);
+    }
+    
+    
