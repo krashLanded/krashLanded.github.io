7b07fd8d3f7e2f74e737011b90d43334d0451275
krashLanded <ericquesada02@icloud.com>
2021-12-06 19:43:31 -0800

    working array - holds books

--- library.js
+++ library.js
@@ -1,12 +1,35 @@
 
-    let title = '';
+    const myLibrary =
+    [];
 
-    function named() {
-        alert(userInput.value);
-    }
-    function getTitle() {
+   
+
+    function getValues() {
         title = document.getElementById('title').value;
-        alert(title);
+        author = document.getElementById('author').value;
+        totalPages = document.getElementById('tpages').value;
+        completedPages = document.getElementById('cpages').value;
+        checkStatus = document.getElementById('read-status').checked;
+
+
+        let currentBook = new Book(title, author, totalPages, completedPages, checkStatus);
+
+        myLibrary.push(currentBook);
+
     }
-    
-    
+
+
+
+function Book(title, author, totalPages, completedPages, checkStatus) {
+    this.title = title;
+    this.author = author;
+    this.totalPages = totalPages;
+    this.completedPages = completedPages;
+    this.checkStatus = checkStatus;
+}
+
+
+let exampleBook = new Book('Example Book', 'Eric Q.', '21', '5', false);
+myLibrary.push(exampleBook);
+
+ 
